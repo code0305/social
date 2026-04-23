ad38f76c9464d612d303840d35eac71002fa85c4
Akash M <155293006+code0305@users.noreply.github.com>
2026-04-23 10:42:40 +0530

    Change login route path to '/'

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path = "/login" element={<LoginPage/>}/>
+      <Route path = "/" element={<LoginPage/>}/>
       <Route path = "/register" element={<RegisterPage/>}/>
       <Route path = "/addpost" element={<AddPost/>}/>
       <Route path='/posts' element={<Viewposts/>}/>
